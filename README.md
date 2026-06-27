250d5d1e9bee9a3cf4885961e7cc109551c6b472
Maxwell Morrissey <mmorrissey92@proton.me>
2026-06-27 13:21:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Compress aspect ratio in a given directory
+
+---
+
+##### Usage
+
+COMMAND \[DIRECTORY\] \[PX_WIDTH\]
+
+```sh
+compress-aspect-ratio ./ 720
+```
+
+##### Setup
+
+```sh
+sudo mv compress-aspect-ratio.sh /usr/local/bin/compress-aspect-ratio
+```
---
+++ compress-aspect-ratio.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+[[ -z "$1" || -z "$2" ]] && echo "USAGE: command [dir] [px-width]"
+
+# Removes trailing '/' and extends path
+DIR=$(realpath "$1")
+PX_WIDTH="$2"
+LG_FILE_ID='ORIGINAL_FILE_SIZE-'
+
+for file in $DIR/*; do
+    large_name="$DIR/$LG_FILE_ID$(basename $file)"
+
+    mv $file $large_name
+    ffmpeg -i $large_name -vf "scale=$PX_WIDTH:-2" $file
+done
+
+echo "Delete original files? [y/n]: "
+read -r answer
+
+if [[ "$answer" == "y" ]]; then
+    echo "Deleting original files"
+    rm "$DIR/$LG_FILE_ID"*
+fi
+
+
